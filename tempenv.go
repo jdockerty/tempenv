8723798e3ee7c62a020a9cce9a0ffdeab222f6e1
Jack Dockerty <jdockerty19@gmail.com>
2021-12-13 21:31:42 +0000

    Add Set func for setting temp environment

--- tempenv.go
+++ tempenv.go
@@ -1,5 +1,33 @@
 package tempenv
 
-func Hello() string {
-	return "Hello, World!"
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+
+// Set is used to perform a function call with the setting and unsetting of the target environment variable
+// handled inbetween. This returns a slice of reflect values, which are the outputs, if any, of the function
+// that you passed.
+func Set(actionFunc interface{}, targetVar, tempVar, resetVar string, actionFuncArgs ...interface{}) (*[]reflect.Value, error) {
+
+	switch t := reflect.TypeOf(actionFunc).Kind(); t {
+	case reflect.Func:
+		os.Setenv(targetVar, tempVar)
+		defer os.Setenv(targetVar, resetVar)
+
+		// Collect the passed arguments into a desirable slice that we can use.
+		fnArguments := make([]reflect.Value, len(actionFuncArgs)) 
+		for i, _ := range actionFuncArgs {
+			fnArguments[i] = reflect.ValueOf(actionFuncArgs[i])
+		}
+
+		funcResult := reflect.ValueOf(actionFunc).Call(fnArguments)
+
+		return &funcResult, nil
+
+	default:
+		return nil, fmt.Errorf("the passed action was not of type func, it is not callable")
+	}
+
 }
