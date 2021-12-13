7442c2d96698053aac6272bab3eda48998654d22
Jack Dockerty <jdockerty19@gmail.com>
2021-12-13 21:31:28 +0000

    Add test for temporary environment

--- tempenv_test.go
+++ tempenv_test.go
@@ -3,11 +3,31 @@ package tempenv_test
 import (
 	"github.com/jdockerty/tempenv"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
-func TestHello(t *testing.T) {
+const (
+	testTempEnv = "TEMPENV_TEST"
+	during     = "duringAction"
+	after      = "afterAction"
+)
+
+
+// TestSetEnvValidateFunc asserts whether our test function, which returns the current environment variable, has
+// the appropriate variable set during its execution time.
+func TestSetEnvValidateFunc(t *testing.T) {
 	assert := assert.New(t)
-	expected := "Hello, World!"
-	assert.Equal(expected, tempenv.Hello())
+
+	testerFunc := func(envVar string) string {
+		return os.Getenv(envVar)
+	}
+
+	funcArgument := testTempEnv
+	result, err := tempenv.Set(testerFunc, testTempEnv, during, after, funcArgument)
+	assert.Nil(err)
+
+	actual := (*result)[0].String() // We know that our function returns a string
+	assert.Equalf(during, actual, "Expected %s string, got %s\n", during, actual)
+	assert.Equal(after, os.Getenv(testTempEnv), "Expected %s, got %s\n", after, actual)
 }
